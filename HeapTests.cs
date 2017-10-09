66cf6a6dde991d07948b253514b5258a1b7fb178
Banana0042 <Banana0042@gmail.com>
2017-10-09 09:45:05 -0600

    Heap Unit Tests added
    Heap tests continually fail because display is null.
    No further testing of the heaps can be done until fixed.

---
+++ HeapTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheGameOfNim;
+using System.Collections.Generic;
+
+namespace NimUnitTests {
+
+    [TestClass]
+    public class HeapTests {
+
+        [TestMethod]
+        public void HeapTest() {
+            List<Heap[]> heaps = new List<Heap[]>();
+            heaps.Add(DifficultyExtentions.GetHeaps(Difficulty.EASY));
+            heaps.Add(DifficultyExtentions.GetHeaps(Difficulty.MEDUIM));
+            heaps.Add(DifficultyExtentions.GetHeaps(Difficulty.HARD));
+            foreach(Heap[] h in heaps)
+            {
+                foreach(Heap heap in h)
+                {
+                    Assert.IsFalse(heap.RemoveStones(heap.StonesLeft + 1));
+                }
+            }
+        }
+    }
+}
